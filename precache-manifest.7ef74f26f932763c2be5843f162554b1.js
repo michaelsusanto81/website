de9f98fec98f769ff15551aafa18572d82d7665a
Michael Susanto <michaelsusanto32@gmail.com>
2020-08-20 17:20:56 +0700

    Updates

---
+++ precache-manifest.7ef74f26f932763c2be5843f162554b1.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26527d7c1b7772d3c0f5a28c2cef2432",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cf621713bbd940c388af",
+    "url": "/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "ba0ae61a58a814c5e810",
+    "url": "/static/css/main.faf538c8.chunk.css"
+  },
+  {
+    "revision": "cf621713bbd940c388af",
+    "url": "/static/js/2.2caacbca.chunk.js"
+  },
+  {
+    "revision": "d3031346469a8a2ef527f1ad705ea3b7",
+    "url": "/static/js/2.2caacbca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba0ae61a58a814c5e810",
+    "url": "/static/js/main.d1b35059.chunk.js"
+  },
+  {
+    "revision": "ba126735810163618457",
+    "url": "/static/js/runtime-main.3374ac37.js"
+  },
+  {
+    "revision": "b780acdee42b36bd596f0d0831292365",
+    "url": "/static/media/SeaPay-Compfest.b780acde.jpg"
+  },
+  {
+    "revision": "535e18c3c365fe43e4b2b9a760ff1c29",
+    "url": "/static/media/TA-ProgFound1.535e18c3.jpg"
+  },
+  {
+    "revision": "53ff22520eb284983c8328c9b0ea54f9",
+    "url": "/static/media/android-logo.53ff2252.png"
+  },
+  {
+    "revision": "8a50b602aa79b19775c22d02a290f51f",
+    "url": "/static/media/flutter-logo.8a50b602.png"
+  },
+  {
+    "revision": "18db35d3c19850d47795e500998b6ea7",
+    "url": "/static/media/internship.18db35d3.jpg"
+  },
+  {
+    "revision": "78866e53c6b97999d1008790c21be73b",
+    "url": "/static/media/java-logo.78866e53.png"
+  },
+  {
+    "revision": "8476e4d7408149fd2138d9b3cb71e339",
+    "url": "/static/media/logo.8476e4d7.svg"
+  },
+  {
+    "revision": "39cc73cc0e654cae60acf041555560c7",
+    "url": "/static/media/michaelsusanto-profile.39cc73cc.png"
+  },
+  {
+    "revision": "5152534aa6abc29debc3831eb0b89916",
+    "url": "/static/media/python-logo.5152534a.png"
+  },
+  {
+    "revision": "ee42cbe115b33ab5a53055b352906e3f",
+    "url": "/static/media/ruby-logo.ee42cbe1.png"
+  }
+]);
