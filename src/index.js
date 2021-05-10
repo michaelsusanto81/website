f1be361fe4b910ae26ff55f122deaec91569eeba
Michael Susanto <michael.susanto81@ui.ac.id>
2021-05-10 22:40:14 +0700

    remove service workers

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import * as serviceWorker from './serviceWorker';
+// import * as serviceWorker from './serviceWorker';
 import App from './containers/App';
 
 ReactDOM.render(
@@ -11,4 +11,4 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-serviceWorker.register();
+// serviceWorker.register();
